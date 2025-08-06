d237c8757c27ed2107eeef7abc47efd9f7105dc1
shuihuahaiyang <2184841402@qq.com>
2025-08-06 20:26:51 +0800

    [提交类] 修改 [提交内容] 注释主线程等待代码

---
+++ src/main/java/com/example/lnacos/duoxiangcheng.java
@@ -0,0 +1,22 @@
+package com.example.lnacos;
+
+public class duoxiangcheng {
+    public static void sleep(){
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void main(String[] args) throws InterruptedException {
+        Thread test = new Thread(() -> {
+            while (true) {
+                System.out.println("test");
+                duoxiangcheng.sleep();
+            }
+        });
+        test.start();
+//        test.join();
+//        System.out.println("结束");
+    }
+}
